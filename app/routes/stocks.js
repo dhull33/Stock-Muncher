144f5bd0b8e500c5913ff56cbb65d6396402d98a
Adrian Jablonski <jablonski.adrian1@gmail.com>
2018-04-17 10:28:15 -0500

    Got API call to return multiple data values

--- app/routes/stocks.js
+++ app/routes/stocks.js
@@ -6,11 +6,8 @@ var connectionString = 'postgres://localhost:5432/stocks';
 var db = pgp(connectionString);
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({extended :false}));
-var $ = require("jquery");
 var fetch = require('node-fetch');
-
-// const cheerio = require('cheerio');
-// const $ = cheerio;
+var request = require('request');
 
 var options = {
     promiseLib : promise
@@ -18,7 +15,6 @@ var options = {
 
 router.get('/stocks',function(req,res){
 
-    // fetch artists from the database
     db.any('SELECT * FROM stocks').then(function(data){
       // res.render(page to render, object to pass to the page)
       res.render('stocks',{'stocks' : data});
@@ -26,86 +22,43 @@ router.get('/stocks',function(req,res){
   })
 
 router.post('/stocks',function(req,res){
-    // Picked up by form name from add_to_database.ejs file
     function update_stock_price() {
       (function() {
           console.log("Submitted")
           var url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
           var symbol = req.body.stock_symbol;
-          var url2 = "&interval=15min&apikey="
+          var url2 = "&interval=1min&apikey="
           var apiKey = "3SW8F3VSYVSP0VAZ";
           // Retreives individual stock prices
+          var stockPrice;
           console.log(symbol);
-          
-          fetch(url + symbol + url2 + apiKey)
-            .then(res => {
-              var response = res.json();
-              console.log(response["Time Series (15min)"]["2018-04-16 14:30:00"])
-            }) 
-            // .then(json => console.log(json["Time Series (15min)"]["2018-04-16 14:30:00"]))
-            //.then(json => console.log(json["Meta Data"]["6. Time Zone"]))
-            // .then(body => console.log(body["Time Series (15min)"]["2018-04-16 14:30:00"]));
 
-            // var timeSeries15 = response["Time Series (15min)"];
-            // console.log(timeSeries15);
-            // var currentDateData = Object.keys(timeSeries15)[0];
-            // console.log(currentDateData);
-            // var timeZone = response["Meta Data"]["6. Time Zone"]
-            // console.log(timeSeries15[currentDateData]);
-            // console.log(currentDateData)
-            // var stockPrice = timeSeries15[currentDateData]["4. close"];
-            // $("#price").html(stockPrice);
-            // $("#currentDate").html(currentDateData + " " + timeZone);
-            
-          // catch(err) {
-          //   console.log("Error");
-            
-          // }
+          fetch(url + symbol + url2 + apiKey)
+            .then(res => res.json())
+            .then(json => {
+              // Assigns response to a variable so that we could access multiple data fields from JSON response
+              var stockData = json;
+              var timeSeries15 = stockData["Time Series (1min)"];
+              var currentDateData = Object.keys(timeSeries15)[0];
+              stockPrice = timeSeries15[currentDateData]["4. close"];
+              var timeZone = stockData["Meta Data"]["6. Time Zone"]
+              console.log(stockPrice);
+              console.log(currentDateData + " " + timeZone)
           
-        
-        // json
-          // $.get(url + symbol + url2 + apiKey).done(function(response) {
-          //     console.log("Success");
-          //     alert("Success")
-          //     updateUISuccess(response);
-          // }).fail(function(error) {
-          //     console.log("Failed");
-          //     alert("Failed");
-          //     updateUIError();
-          // })
-  
-          // // handle success
-          // function updateUISuccess(response) {
-          //     console.log(response);
-          //     var timeSeries15 = response["Time Series (15min)"];
-          //     var currentDateData = Object.keys(timeSeries15)[0];
-          //     var timeZone = response["Meta Data"]["6. Time Zone"]
-          //     console.log(timeSeries15[currentDateData]);
-          //     console.log(currentDateData)
-          //     var stockPrice = timeSeries15[currentDateData]["4. close"];
-          //     $("#price").html(stockPrice);
-          //     $("#currentDate").html(currentDateData + " " + timeZone);
-          // }
-  
-          // // handle error
-          // function updateUIError() {
-          //     console.log("Fail");
-          // }
-  
+            })
+            console.log(stockPrice)
+        // currentPrice = 4;
+      
+        db.none('INSERT INTO stock_purchase(stock_symbol, purchase_price_each) values($1, $2)',[symbol, stockPrice]).then(function(){
+      
+          db.any('SELECT * FROM stocks').then(function(data){
+            // res.render(page to render, object to pass to the page)
+            res.render('stocks', {'stocks' : data});
+          })
+            
+        })
       })();
 
-      // let stockName = symbol;
-      // let currentPrice = stockPrice;
-      // // currentPrice = 4;
-    
-      // db.none('INSERT INTO stocks(stock_symbol, current_price) values($1, $2)',[stockName, currentPrice]).then(function(){
-    
-        // db.any('SELECT * FROM stocks').then(function(data){
-        //   // res.render(page to render, object to pass to the page)
-        //   res.render('stocks', {'stocks' : data});
-        // })
-    
-      // })// end of 'then' promise
     }
     update_stock_price()
   })
