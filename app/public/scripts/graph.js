1a5617080f5d62698519db9dac7fb1a796c07ce0
David Hull <david.hull1@utexas.edu>
2018-04-22 13:58:48 -0500

    Added graphs to autocomplete_list.js

--- app/public/scripts/graph.js
+++ app/public/scripts/graph.js
@@ -2,21 +2,20 @@
 google.charts.load('current', {packages: ['corechart', 'controls']});
 
 google.charts.setOnLoadCallback(function(){
-    $("#stockPriceUpdate").on("click", function(e) {
-        console.log(e.target);
-        drawChart();
+    $("#stockPriceUpdate").on("click", function() {
+        //console.log(e.target);
     });
 });
 
 
 
 
-function drawChart() {
+/*function drawChart() {
     (function() {
         console.log("Submitted")
         var url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="
-        var symbol = document.getElementById("stockSymbol").value.toUpperCase() + ":"; // Added : to end in case autocomplete is not used and only stock symbol is used
-        symbol = symbol.substr(0, symbol.indexOf(":")); // Keeps symbol only
+        var symbol = document.getElementById("stockSymbol").value.toUpperCase(); // Added : to end in case autocomplete is not used and only stock symbol is used
+        //symbol = symbol.substr(0, symbol.indexOf(":")); // Keeps symbol only
         console.log(symbol);
 
         //specifies time interval
@@ -37,15 +36,13 @@ function drawChart() {
         // handle success
         function updateUISuccess(response) {
             console.log(response);
-            var timeSeries15 = response["Time Series (1min)"];
+            var timeSeries15 = response["Time Series (15min)"];
 
             //Use object.keys to access data
             var currentDateData = Object.keys(timeSeries15)[0];
             var timeZone = response["Meta Data"]["6. Time Zone"];
             //console.log(timeSeries15[currentDateData]);
 
-            console.log('test')
-            console.log(currentDateData.length);
             var stockPrice = timeSeries15[currentDateData]["4. close"];
 
             // ### CHARTZZZZZZZ ###
@@ -147,4 +144,6 @@ function drawChart() {
     })();
 
 };
+
+*/
 
