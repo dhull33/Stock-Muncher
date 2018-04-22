38c81a7b6e057b6e9675cb18e98b3a401d02b872
Jake Anderson <sk8er4lifejma@hotmail.com>
2018-04-22 14:20:33 -0500

    userpage displays username, index links to signup/signin

--- app/routes/userPage.js
+++ app/routes/userPage.js
@@ -24,12 +24,14 @@ var apiKey = "3SW8F3VSYVSP0VAZ";
 
 router.get('/userPage',function(req,res){
     var symbol = req.body.stock_symbol
+    var user= req.user.username
     db.any('SELECT * FROM stocks').then(function(data){
 
         // res.render(page to render, object to pass to the page)
         res.render('userPage',{
             pageTitle: "User's Page",
-            stocks : data
+            stocks : data,
+            user : user
         });
     })
 })
